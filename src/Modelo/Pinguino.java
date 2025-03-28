1c666f3beef3be29b1700b4d203a38023b155bed
amiyasarkaz <marccunillera2006@gmail.com>
2025-03-28 16:11:03 +0100

    Commit Classe Usuarios, Tablero, Pinguino, Foca_CPU
    He instanciat les variables he definit els constructors i he creat els mètodes necessaris per cada classe

--- src/Modelo/Pinguino.java
+++ src/Modelo/Pinguino.java
@@ -1,10 +1,51 @@
 package Modelo;
 
-public class Pinguino {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Pinguino extends Usuarios {
+	
+	// Hem instanciat les variables
+	private String color;
+	private String nombre;
+	
+	// Hem definit el constructor
+	public Pinguino ( int ID, String color, String nombre) {
+		super(ID);
+		this.color = color;
+		this.nombre = nombre;
 	}
+
+	// Geters y seters
+    public String getcolor() {
+        return color;
+    }
+
+    public void setcolor(String color) {
+        this.color = color;
+    }
+    
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    // ToString per mostra el color i el nom
+    public String toString() {
+        return super.toString() + ", Color: " + color + "Nombre" + nombre;
+    }
+    
+    // Metodes de la classe pinguino
+    public void TirarDado() {
+    	
+    }
+    
+    public void UsarBolasNieve() {
+    	
+    }
+    
+    public void AlimentarFoca() {
+    	
+    }
 
 }
