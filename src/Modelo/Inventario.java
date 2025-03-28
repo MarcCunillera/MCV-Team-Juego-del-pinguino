55a575b79120b49dfe3433307d8c65fe42621f9b
MarcValentines <marcvalentinesmiro@gmail.com>
2025-03-28 16:23:45 +0100

    Inventario -> Plantilla base
    Plantilla básica per generar el inventari dels jugadors

--- src/Modelo/Inventario.java
+++ src/Modelo/Inventario.java
@@ -1,5 +1,35 @@
 package Modelo;
 
 public class Inventario {
-
+	private int IDinv;
+	private int espacioInv;
+	
+	//constructor de la clase
+	public Inventario(int IDinv, int espacioInv) {
+		this.IDinv = IDinv;
+		this.espacioInv = espacioInv;
+	}
+	
+	//métodos get
+	public int getIDinv() {
+		return IDinv;
+	}
+	
+	public int getespacioInv() {
+		return espacioInv;
+	}
+	
+	//métodos set
+	public void setIDinv(int IDinv) {
+		this.IDinv = IDinv;
+	}
+	
+	public void setespacioInv(int espacioInv) {
+		this.espacioInv = espacioInv;
+	}
+	
+	//método toString
+	public String toString() {
+		return "Espacio del inventario: " + espacioInv;
+	}
 }
