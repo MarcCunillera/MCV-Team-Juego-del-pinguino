dcb7d621f7d81bf9a5c53dc4df2598e5eda00317
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-03-28 17:02:47 +0100

    4. Clase Objetos ( Pakage Controlador )
    En esta clase lo que he creado han sido los atributos de  la clase que serian los objetos, depsues el contructor de la clase y los getters i los setters de cada uno de los objetos ademas de unos breves comentarios

--- src/Controlador/Objetos.java
+++ src/Controlador/Objetos.java
@@ -1,10 +1,100 @@
 package Controlador;
 
 public class Objetos {
+	
+	/*
+	 * Atributos de los Ojectos de los Juegos
+	 */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int Pez;
+	private int Nieve;
+	private int DadoRapido;
+	private int DadoLento;
+	private int DadoNormal;
+	
+	/*
+	 * Constructor de la Clase Objetos
+	 */
+	
+	public Objetos(int Pez,int Nieve,int DadoRapido, int DadoLento, int DadoNormal) {
+		
+		this.Pez = Pez;
+		this.Nieve = Nieve;
+		this.DadoRapido = DadoRapido;
+		this.DadoLento = DadoLento;
+		this.DadoNormal = DadoNormal;
+		
 	}
+	
+	/*
+	 * Getters y Setters de los objetos
+	 */
+	
+	public int getPez(){
+		
+		return Pez;
+		
+	}
+	
+	public void setPez(int Pez) {
+		
+		this.Pez = Pez;
+		
+	}
+	
+	public int getNieve(){
+		
+		return Nieve;
+		
+	}
+	
+	public void getNieve(int Nieve) {
+		
+		this.Nieve = Nieve;
+		
+	}
+	
+	
+	public int getDadoRapido(){
+		
+		return DadoRapido;
+		
+	}
+	
+	public void setDadoRapido(int DadoRapido) {
+		
+		this.DadoRapido = DadoRapido;
+		
+	}
+	
+	
+	public int getDadoLento(){
+		
+		return DadoLento;
+		
+	}
+	
+	public void setDadoLento(int DadoLento) {
+		
+		this.DadoLento = DadoLento;
+		
+	}
+	
+	
+	public int getDadoNormal(){
+		
+		return DadoNormal;
+		
+	}
+	
+	public void setDadoNormal(int DadoNormal) {
+		
+		this.DadoNormal = DadoNormal;
+		
+	}
+	
+
+
+	
 
 }
