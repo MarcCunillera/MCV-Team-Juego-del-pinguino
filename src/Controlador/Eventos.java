e743b74cedd1be087df39747f7e2bbe7a9bcebaa
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-03-28 16:08:31 +0100

    2. Clase Eventos ( Package Controlador )
    En este parte lo que he hecho ha sido crear las variables de la Clase Eventos y su constructor con los getter y el setter de la clase ademas de unos Comentarios y ademas tiene los siguientes mnetedos:
    
    - Trineo
    - CasillaInterogante
    - CasillaQuebradiza
    - Guerra de Jugadores
    - Oso
    - Motos de Nieve

--- src/Controlador/Eventos.java
+++ src/Controlador/Eventos.java
@@ -2,9 +2,74 @@ package Controlador;
 
 public class Eventos {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/*
+	 * Creacion de la variable privada
+	 */
+	
+	private int idEvento;
+	
+	/*
+	 * Constructor de las Clase Evento
+	 */
+	
+	public Eventos(int idEvento) {
+		
+		this.idEvento = idEvento;
+		
+	}
+	
+	/*
+	 * Getter y Setter del Id Evento
+	 */
+	
+	public int getidEvento() {
+		
+		return idEvento;
+			
+	}
+	public void setidEvento(int idEvento) {
+		
+		this.idEvento = idEvento;
+		
+	}
+	
+	public void AgujeroHielo() {
+		
+	}
+	
+	/*
+	 * Metodos de los tipos de Casillas y Objetos
+	 */
+	
+	
+	public void Trineo() {
+	
+	}
+	
+	
+	public void CasillaInterogante() {
+	
+	}
+	
+	
+	public void CasillaQuebradiza() {
+	
+	}
+	
+	
+	public void GuerraJugadores() {
+	
+	}
+	
+	
+	public void Oso() {
+	
+	}
+	
+	
+	public void MotosNieve() {
+		
 	}
 
+	
 }
