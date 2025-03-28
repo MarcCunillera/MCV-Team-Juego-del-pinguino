0c0ce4fdb80428d1433d016fbd8a8d31ea5b7b3d
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-03-28 15:54:17 +0100

    1. Comit de la Clase Casilla ( Pakage Controladro
    En esta clase lo que he hecho ha sido crear las variables privadas y su constructor de la clase con los getters i los setter ademas de crear la funcion interaccion evento y los cometarios

--- src/Controlador/Casilla.java
+++ src/Controlador/Casilla.java
@@ -2,9 +2,59 @@ package Controlador;
 
 public class Casilla {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int idCasilla;
+	private int TipoCasilla;
+	private int JugadoresCasilla;
+	
+	public Casilla (int idCasilla, int TipoCasilla, int JugadoresCasilla) {
+		
+		this.idCasilla = idCasilla;
+		this.TipoCasilla = TipoCasilla;
+		this.JugadoresCasilla = JugadoresCasilla;
+		
+	}
+	
+	public int getidCasilla() {
+		
+		return idCasilla;
+		
+	}
+	public void getidCasilla(int idCasilla) {
+		
+		this.idCasilla = idCasilla;
+		
+	}
+	
+	
+	public int getTipoCasilla() {
+		
+		return TipoCasilla;
+		
+	}
+	public void getTipoCasilla(int TipoCasilla) {
+		
+		this.TipoCasilla = TipoCasilla;
+		
+	}
+	
+	
+	public int getJugadoresCasilla() {
+		
+		return JugadoresCasilla;
+		
+	}
+	public void getJugadoresCasilla(int JugadoresCasilla) {
+		
+		this.JugadoresCasilla = JugadoresCasilla;
+		
 	}
 
+	
+	public void InteracionEvento () {
+		
+		
+		
+	}
+	
+	
 }
