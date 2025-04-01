315912e98a31b6fb11eb5b24bd44eb9781be8005
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-04-01 17:07:55 +0200

    Casilla Interrogante
    En este método lo que he si cambiar mi codigo y añadirlo a la rama ademas de que el bucle while lo que hara va a ser que escoga una de las opcionbes que hay en los ventos y apartir de los eventos lo siguiente es lo que le va a pasar al jugador lo que le llamamos a las metodos de cada evento para que pase en esa casilla

--- src/Controlador/Eventos.java
+++ src/Controlador/Eventos.java
@@ -48,17 +48,38 @@ public class Eventos {
 		int AjHielo = 1;
 		int Trineu = 2;
 		int oso = 3 ;
-		int Motnieve = 4 ;
 
-		int Evento = eventAL.nextInt(4)+1 ;
+		int Evento = eventAL.nextInt(3)+1 ;
 		
-		while () {
+		// Cambiar Obligatoriamnte ( esta de momento el true )
+		
+		while (true) {
 			
-			if () {
+			if(AjHielo == 1) {
+				
+				//AjHielo.AgujeroHielo();
+				
+				System.out.println("Has caido dentro de un agujero de hielo !!!");
 				
 			}
+			
+			else if(Trineu == 2){
+				
+				//Trienu.Trineo();
+				
+				System.out.println("De trineo a trineo !!!");
+				
+			}
+			
+			else if(oso == 3) {
+				
+				System.out.println("Has sido atacado por un Oso !!!");
+				
+				System.out.println("Vuelves al principio del juego");
+				
+			}
+			
 		}
-		
 		
 		
 	}
@@ -81,7 +102,7 @@ public class Eventos {
 	
 	public void Oso() {
 	
-		int JugadorAtacado
+		int JugadorAtacado;
 		
 		
 	}
