de538794b73058fb36a50cc858e0fd68e9a28c7f
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-04-01 17:02:27 +0200

    Borrado codigo del main
    EL comit me equivocado y lo he hecho dentro del main antes que mi rama

--- src/Controlador/Eventos.java
+++ src/Controlador/Eventos.java
@@ -43,47 +43,6 @@ public class Eventos {
 	 */
 	
 	public void CasillaInterogante(int Casilla ) {
-		
-		Random eventAL = new Random();
-		
-		int AjHielo = 1;
-		int Trineu = 2;
-		int oso = 3 ;
-
-		int Evento = eventAL.nextInt(3)+1 ;
-		
-		// Cambiar Obligatoriamnte ( esta de momento el true )
-		
-		while (true) {
-			
-			if(AjHielo == 1) {
-				
-				//AjHielo.AgujeroHielo();
-				
-				System.out.println("Has caido dentro de un agujero de hielo !!!");
-				
-			}
-			
-			else if(Trineu == 2){
-				
-				//Trienu.Trineo();
-				
-				System.out.println("De trineo a trineo !!!");
-				
-			}
-			
-			else if(oso == 3) {
-				
-				System.out.println("Has sido atacado por un Oso !!!");
-				
-				System.out.println("Vuelves al principio del juego");
-				
-			}
-			
-			if () {
-				
-			}
-		}
 	
 		
 	}
