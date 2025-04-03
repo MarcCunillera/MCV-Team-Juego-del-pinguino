d64024c4c682191018f369b369a328f34e9d2060
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-04-03 15:42:52 +0200

    Atributos, Constructores, Getters y Setters
    En esta clase lo que he hecho a si crear los atributos privados y el constructor de los atributos y los getters y los setter de cada uno de los atributos que haya

--- src/Controlador/Objetos.java
+++ src/Controlador/Objetos.java
@@ -1,5 +1,40 @@
 package Controlador;
 
 public class Objetos {
+	
+	// Atributos de los Objetos
+	
+	private int pez;
+	private int nieve;
+	
+	// Constructor de los Atributos
+	
+	public Objetos(int pez, int nieve) {
+	
+		this.pez = pez;
+		this.nieve = nieve;
+		
+	}
+	
+	// Getters y Setters de los atributos
 
+	public int getPez() {
+		return pez;
+	}
+
+	public void setPez(int pez) {
+		this.pez = pez;
+	}
+
+	public int getNieve() {
+		return nieve;
+	}
+
+	public void setNieve(int nieve) {
+		this.nieve = nieve;
+	}
+	
+	
+	
+	
 }
