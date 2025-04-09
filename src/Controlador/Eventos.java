3a902606942d668e182eb68d2a3b651ea50b3558
Carlos <131690161+Yosoyina@users.noreply.github.com>
2025-04-09 19:37:06 +0200

    Eliminacion de clases y Moviendolo en los eventos
    Hemos eliminado la clase dado y objetos ya que no tiene que ver en esas dos clases partidadas ya que lo tenemos que poner todo en evento popr que en esa clase esta mas relacionado que es esas dos classes que no tiene nada que ver.

--- src/Controlador/Eventos.java
+++ src/Controlador/Eventos.java
@@ -8,31 +8,75 @@ public class Eventos {
 	 */
 	
 	private int idEvento;
+	private int DadoRapido;
+	private int DadoLento;
+	private int pez;
+	private int nieve;
 	
 	/*
 	 * Constructor de las Clase Evento
 	 */
 	
-	public Eventos(int idEvento) {
+	public Eventos(int idEvento, int dadoRapido,  int dadoLento, int pez, int nieve) {
 		
 		this.idEvento = idEvento;
-		
+		this.DadoRapido = dadoRapido;
+		this.DadoLento = dadoLento;
+		this.pez = pez;
+		this.nieve = nieve;
 	}
 	
 	/*
 	 * Getter y Setter del Id Evento
 	 */
 	
-	public int getidEvento() {
-		
+
+	public int getIdEvento() {
 		return idEvento;
-			
 	}
-	public void setidEvento(int idEvento) {
-		
+
+	public void setIdEvento(int idEvento) {
 		this.idEvento = idEvento;
-		
 	}
+
+	public int getDadoRapido() {
+		return DadoRapido;
+	}
+
+	public void setDadoRapido(int dadoRapido) {
+		DadoRapido = dadoRapido;
+	}
+
+	public int getDadoLento() {
+		return DadoLento;
+	}
+
+	public void setDadoLento(int dadoLento) {
+		DadoLento = dadoLento;
+	}
+
+	public int getPez() {
+		return pez;
+	}
+
+	public void setPez(int pez) {
+		this.pez = pez;
+	}
+
+	public int getNieve() {
+		return nieve;
+	}
+
+	public void setNieve(int nieve) {
+		this.nieve = nieve;
+	}
+	
+	public void InteraccionDado() {
+	
+	}
+	
+	
+	
 	
 
 
